6ba1a20bda75e3a7c383016dc8a6d21d14f11d3e
ltcervan <ltcervan@gmail.com>
2024-02-07 19:26:53 -0800

    CHORE created requirements.txt

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include ## added the include
 
+
 urlpatterns = [
     path('polls/', include('polls.urls')), # added from polls/urls.py 
     path('admin/', admin.site.urls),
--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('choice_text', models.CharField(max_length=200)),
-                ('votes', models.IntegerField(default=0)),
+                # ('votes', models.IntegerField(default=0)),
                 ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.question')),
             ],
         ),
